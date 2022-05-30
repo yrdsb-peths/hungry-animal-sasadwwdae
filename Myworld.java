a3e18804879d2d91bae3927c69380fdc7fd4ba35
asdafwr <105795748+sasadwwdae@users.noreply.github.com>
2022-05-30 09:41:05 -0400

    Create Label class and add in the world

--- Myworld.java
+++ Myworld.java
@@ -19,7 +19,10 @@ public class Myworld extends World
         super(600, 400, 1); 
         
         Elephant elephant = new Elephant();
-        addObject(elephant,300,200);
+        addObject(elephant,300,300);
+        
+        Label scoreLabel = new Label(0, 80);
+        addObject(scoreLabel,50,50);
         
         createApple();
     }
